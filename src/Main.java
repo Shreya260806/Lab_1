ce526021e37907051e1c835109daa4646b1d9a5c
Shreya Choudhary 1882 <24051882@kiit.ac.in>
2025-12-12 12:48:06 +0530

    Question 1

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+class print
+        {
+            public static void main(String[] args)
+            {
+                System.out.println("Shreya Choudhary \n24051882 \nCSE-33 \nComputer Science");
+            }
+        }
