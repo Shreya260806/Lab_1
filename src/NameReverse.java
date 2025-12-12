63161b23c7a51042b390a183a01a33200681ae4d
Shreya Choudhary 1882 <24051882@kiit.ac.in>
2025-12-12 13:36:24 +0530

    Question 5

---
+++ src/NameReverse.java
@@ -0,0 +1,7 @@
+public class NameReverse {
+    public static void main(String[] args) {
+        String firstName = "Shreya";
+        String lastName = "Choudhary";
+        System.out.println(lastName + " " + firstName);
+    }
+}
